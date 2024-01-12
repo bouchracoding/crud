f4a6c7f6a7f5c8387b8a49324f97c78e7abedc02
bouchracoding <152641287+bouchracoding@users.noreply.github.com>
2024-01-12 18:00:09 +0100

    first commit

---
+++ crud-react-redux-toolkit/src/Data.jsx
@@ -0,0 +1,19 @@
+export const userList=[
+    {
+        name:"ali",
+        email:"ali@gmail.com",
+        id:1
+    },
+    {
+        name:"mina",
+        email:"mina@gmail.com",
+        id:2
+    },
+    {
+        name:"fati",
+        email:"fati@gmail.com",
+        id:3
+    }
+    
+
+]
